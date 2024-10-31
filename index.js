363ecbbac935fbe5967ef132bc22862c85abcc83
Jdebbie254 <djalagat148@gmail.com>
2024-10-31 16:25:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let name = "peter"
+
+let num = 2.44;
+
+let bool = false;
+
+let array = [1,2,3,4,5,6,7,8,9,10
+
+let product = {
+id: 1
+name: "soap"
+price: 100
+category
+let Area= calculateArea( w,1)
